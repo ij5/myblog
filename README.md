349c71c3961bc49682e6328f161bc33e198d4450
Jaehee Lee <55607202+ij5@users.noreply.github.com>
2024-06-15 18:13:14 +0900

    Quartz sync: Jun 15, 2024, 6:13 PM

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
 # myblog
+=======
+# MY BLOG
+[w/Quartz](https://github.com/jackyzha0/quartz)
+>>>>>>> bfb5803 (Quartz sync: Jun 15, 2024, 6:13 PM)
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "이이재희",
     enableSPA: true,
-    enablePopovers: true,
+    enablePopovers: false,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "ko-KR",
+    baseUrl: "ij5.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
